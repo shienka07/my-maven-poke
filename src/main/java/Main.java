55c1658cd21e2a8b3ae07bdd5465564e5d8c430b
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-03-05 16:20:16 +0900

    이미지 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
 import java.util.Random;
 
 public class Main {
@@ -12,6 +17,22 @@ public class Main {
         Random rand = new Random();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(rand.nextInt(1,152)))).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+//        System.out.println(response.body());
+        ObjectMapper mapper = new ObjectMapper();
+        Pokemon pokemon = mapper.readValue(response.body(),Pokemon.class);
+        System.out.println(pokemon.sprites.frontDefault);
+
     }
 }
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Pokemon {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Sprites {
+        @JsonProperty("front_default")
+        public String frontDefault;
+    };
+
+    public Sprites sprites;
+}
+
