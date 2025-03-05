8212e24afd0c58af003ecf20bd53c0ab0507a128
qus0in <qus0in@gmail.com>
2025-03-05 16:38:39 +0900

    끗

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+<<<<<<< HEAD
 import java.util.Objects;
+=======
+import java.util.List;
+>>>>>>> 80e9590 (끗)
 import java.util.Random;
 
 public class Main {
@@ -20,9 +24,29 @@ public class Main {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 //        System.out.println(response.body());
         ObjectMapper mapper = new ObjectMapper();
+<<<<<<< HEAD
         Pokemon pokemon = mapper.readValue(response.body(),Pokemon.class);
         System.out.println(pokemon.sprites.frontDefault);
 
+=======
+//        System.out.println(response.body());
+        Pokemon pokemon = mapper.readValue(response.body(), Pokemon.class);
+        System.out.println(pokemon.sprites.frontDefault);
+
+        String apiUrl2 = "https://pokeapi.co/api/v2/pokemon-species/%d";
+        HttpRequest request2 = HttpRequest.newBuilder().uri(URI.create(apiUrl2.formatted(pokemonId))).GET().build();
+        HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+//        System.out.println(response2.body());
+        PokemonSpecies pokemonSpecies = mapper.readValue(response2.body(), PokemonSpecies.class);
+        // 여러 이름이 나와 -> filter -> language.name == ko -> map -> el.name
+        // 1개 -> findFirst -> Optional (Wrapper) -> 없으면 오류내 (orElseThrow)
+        System.out.println(pokemonSpecies.names
+                .stream()
+                .filter(el -> el.language.name.equals("ko"))
+                .map(el -> el.name)
+                .findFirst().orElseThrow());
+>>>>>>> 80e9590 (끗)
     }
 }
 
@@ -32,7 +56,27 @@ class Pokemon {
     public static class Sprites {
         @JsonProperty("front_default")
         public String frontDefault;
+<<<<<<< HEAD
     };
 
     public Sprites sprites;
 }
+=======
+    }
+    public Sprites sprites;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true) // 추가
+class PokemonSpecies {
+    @JsonIgnoreProperties(ignoreUnknown = true) // 추
+    public static class Name {
+        public String name;
+        @JsonIgnoreProperties(ignoreUnknown = true) //
+        public static class Language {
+            public String name;
+        }
+        public Language language;
+    }
+    public List<Name> names;
+}
+>>>>>>> 80e9590 (끗)
