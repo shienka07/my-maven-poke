7f01da7bf44a5b082af7286aaad8e03868c81124
qus0in <qus0in@gmail.com>
2025-03-05 15:49:22 +0900

    악

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Random;
 
 public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String apiUrl = "https://pokeapi.co/api/v2/pokemon/%d";
+        HttpClient client = HttpClient.newHttpClient();
+        Random rand = new Random();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl.formatted(rand.nextInt(1,152)))).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 }
